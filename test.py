8c884b127505ef8133898c7b0635dff8f24786e3
Jinho Lee <jnonyx@gmail.com>
2021-03-23 00:40:47 -0400

    Create test.py

---
+++ test.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+import os
+
+headers = ["Content-type: text/html"]
+qs = os.environ['QUERY_STRING']
+
+def sendHeaders():
+    for h in headers:
+        print h
+    print "\n"
+
+def sendForm():
+    print '''
+    <html>
+      <body>
+          <form action='cgi-bin/hellobetter.py' method='get'>
+              <label for="myname">Enter Your Name</label>
+              <input id="myname" type="text" name="firstname" value="Nada" />
+              <input type="submit">
+          </form>
+      </body>
+    </html>
+    '''
+
+def sendPage(name):
+    print '''
+    <html>
+      <body>
+        <h1>Hello {0}</h1>
+      </body>
+    </html>
+    '''.format(name)
+
+if not qs:
+    sendHeaders()
+    sendForm()
+else:
+    if 'firstname' in qs:
+        name = qs.split('=')[1]
+    else:
+        name = 'No Name Provided'
+    sendHeaders()
+    sendPage(name)
